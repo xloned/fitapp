783afd13719acd68044bf07e86b12c28083d8818
xloned <xloned@users.noreply.github.com>
2026-02-25 13:47:26 +0300

    fix

--- app/bot/handlers/diary.py
+++ app/bot/handlers/diary.py
@@ -58,7 +58,7 @@ def format_week_text(monday: datetime.date, entries_by_date: dict) -> str:
             for e in entries:
                 content += f"• {e.title}\n"
 
-        text += f"<blockquote><b>{day_name} ~ {day.strftime('%d.%m')}</b></blockquote>\n<{content}\n"
+        text += f"<blockquote><b>{day_name} ~ {day.strftime('%d.%m')}</b>\n{content}\n</blockquote>"
 
     return text
 
